8c2e0fef2f1655b3f5276ad167e82cbaf7b96e01
liuhui <liuhui@youzu.com>
2018-05-10 11:47:15 +0800

    --

---
+++ my-project/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <wordActivity></wordActivity>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import wordActivity from '. /components/wordActivity.vue';
+// console.log(activity,222)
+export default {
+  name: 'App',
+    components:{
+      wordActivity
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ my-project/src/components/wordActivity.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container">
+      <ul class="activity-list" id="example-1" >
+        <!-- <li v-for="item in items" >
+            <img :src="item.src">
+            <span> {{item.message}} </span>
+        </li> -->
+        <li>dddd</li>
+      </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'wordActivity',
+  data () {
+    return {
+      items: [
+        { src: 'http://upload.youzu.com/youzu/2018/0427/100817272.jpg', message: '标题最多15个字标题最多15个字' },
+        { src: 'http://upload.youzu.com/youzu/2018/0427/100817272.jpg', message: '标题最多15个字标题最多15个字' },
+        { src: 'http://upload.youzu.com/youzu/2018/0427/100817272.jpg', message: '标题最多15个字标题最多15个字' }
+      ]
+    }
+  }
+}
+</script>
+<style>
+  .container {
+    width: 7.5rem;
+    margin: 0 auto;
+  }
+  .activity-list li{
+    background: url(src/assets/activityBg.png) no-repeat;
+    width: 5.83rem;
+    height: 3.94rem;
+    margin:0 auto;
+  }
+  .activity-list li img{
+    width:5.5rem;
+    height:2.7rem;
+    display: block;
+    margin:0 auto;
+  }
+  .activity-list li span{
+    font-size:0.24rem;
+    color:#921E07;
+    font-weight:700;
+    height:0.92rem;
+    line-height:0.92rem;
+    text-align: center;
+  }
+</style>
+
